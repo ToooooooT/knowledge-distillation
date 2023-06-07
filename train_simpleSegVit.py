29275236da97adf27ebed268b0724ea994fd653b
tooooooot <linyugung@gmail.com>
2023-06-08 04:27:15 +0800

    Add mIoU and cross entropy.

--- train_simpleSegVit.py
+++ train_simpleSegVit.py
@@ -1,15 +1,22 @@
 import argparse
 import os
 import random
-import tqdm
+from tqdm import tqdm
+
+import numpy as np
 
 from models.SimpleSegViT.SimpleSegViT import SimpleSegViT
+from models.UNet.AttUNet import AttUNet
+from models.UNet.UNet import UNet
 from torchsummary import summary
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
+
+from torchvision import transforms
 
 from mmengine import Config
 from mmengine.runner import Runner
@@ -26,17 +33,18 @@ def parse_args():
     parser.add_argument('--config', default='./configs/segvit/segvit_vit-l_jax_640x640_160k_ade20k.py')
     parser.add_argument('--checkpoint', default='./pretrained/vit_large_ade.pth')
     parser.add_argument('--lr', default=5e-4, type=float, help='learning rate')
-    parser.add_argument('--batch_size', default=4, type=int, help='batch size')
+    parser.add_argument('--batch_size', default=1, type=int, help='batch size')
     parser.add_argument('--cuda', default=False, action='store_true')  
     parser.add_argument('--epochs', default=100, type=int, help='number of epochs to trainkj')  
     parser.add_argument('--eval_freq', default=4, type=int, help='test frequency')  
     parser.add_argument('--model_dir', default='', type=str, help='model directory')  
-    parser.add_argument('--log_dir', default='./logs/SimpleSegVit', type=str, help='base directory to save logs')  
+    parser.add_argument('--model_type', default='AttUnet', type=str, help='student model type')  
+    parser.add_argument('--log_dir', default='./logs/', type=str, help='base directory to save logs')  
     parser.add_argument('--optimizer', default='adam', type=str, help='optimizer to train with')  
     parser.add_argument('--seed', default=1, type=int, help='manual seed')
     # ---------------------- encoder ----------------------
     parser.add_argument('--shrink_idx', default=-1, type=int, help='the layer to shrink')
-    parser.add_argument('--img_size', default=640, type=int, help='the image size')
+    parser.add_argument('--img_size', default=512, type=int, help='the image size')
     parser.add_argument('--encoder_in_channels', default=3, type=int, help='the input channels of encoder')
     parser.add_argument('--encoder_embed_dims', default=1024, type=int, help='the embedding dimension of encoder')
     parser.add_argument('--encoder_num_layers', default=6, type=int, help='number of layers of encoder')
@@ -49,18 +57,77 @@ def parse_args():
     parser.add_argument('--decoder_embed_dims', default=512, type=int, help='the embedding dimensions of decoder')
     parser.add_argument('--decoder_num_heads', default=8, type=int, help='number of head of decoder')
     # use_stages = len(out_indices).
+    parser.add_argument('--teach_w', default=0.5, type=float, help='the loss weight of teacher label')
+    parser.add_argument('--ground_w', default=0.5, type=float, help='the loss weight of ground truth')
 
     args = parser.parse_args()
     return args
 
-def train(teacher, student, img, label, device):
-    img = img.to(device)
-    label = label.to(device)
+
+def intersectionAndUnionGPU(output, target, K, ignore_index=255):
+    # 'K' classes, output and target sizes are N or N * L or N * H * W, each value in range 0 to K - 1.
+    assert (output.dim() in [1, 2, 3])
+    assert output.shape == target.shape
+    output = output.view(-1)
+    target = target.view(-1).to(torch.int64)
+    output[target == ignore_index] = ignore_index
+    intersection = output[output == target]
+    area_intersection = torch.histc(intersection, bins=K, min=0, max=K-1)
+    area_output = torch.histc(output, bins=K, min=0, max=K-1)
+    area_target = torch.histc(target, bins=K, min=0, max=K-1)
+    area_union = area_output + area_target - area_intersection
+    return area_intersection, area_union, area_target
+
+
+def train(teacher: nn.Module, student: nn.Module, img, label, optimizer, args):
     with torch.no_grad():
         teacher_pred = teacher(img)
-        student_pred = student(img)
-    loss_module = ATMLoss(num_classes=150, dec_layers=len(student.encoder.out_indices), loss_weight=1.0)
-    loss_module(teacher_pred, label, 0)
+        teacher_pred['pred'] = (teacher_pred['pred'].argmax(dim=1, keepdim=True) + 1)
+
+    i, u, _ = intersectionAndUnionGPU(teacher_pred['pred'].squeeze(0), label.squeeze(0), 151, ignore_index=0)
+    mIOU = i.sum() / u.sum() # mean IOU, taking (i/u).mean() is wrong
+    print(mIOU)
+
+    # TODO: segvit loss function from paper
+    if isinstance(student, tuple):
+        student1, student2 = student
+        student1_pred = student1(img)
+        student2_pred = student2(img)
+        loss1 = nn.CrossEntropyLoss(student1_pred.view(-1, 150), label.view(-1)) * args.ground_w + \
+                nn.CrossEntropyLoss(student1_pred.view(-1, 150), teacher_pred['pred'].view(-1)) * args.teach_w
+        loss2 = nn.CrossEntropyLoss(student2_pred['pred'].view(-1, 150), label.view(-1)) * args.ground_w + \
+                nn.CrossEntropyLoss(student2_pred['pred'].view(-1, 150), teacher_pred['pred'].view(-1)) * args.teach_w
+    else:
+        if args.model_type != 'simple_segvit':
+            student_pred = student(img)
+            loss = F.cross_entropy(student_pred.permute(0, 2, 3, 1).contiguous().view(-1, 150), label.view(-1)) * args.ground_w + \
+                    F.cross_entropy(student_pred.permute(0, 2, 3, 1).contiguous().view(-1, 150), teacher_pred['pred'].view(-1)) * args.teach_w
+        else:
+            pass
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    loss.detach().cpu()
+    return loss.item()
+
+
+def pred(student, img, label, args):
+    with torch.no_grad():
+        if isinstance(student, tuple):
+            student1, student2 = student
+            student1_pred = student1(img)
+            student2_pred = student2(img)
+        else:
+            if args.model_type != 'simple_segvit':
+                student_pred = student(img)
+            else:
+                pass
+
+    i, u, _ = intersectionAndUnionGPU(student_pred, label, 150, ignore_index=0)
+    mIOU = i.sum()/u.sum() # mean IOU, taking (i/u).mean() is wrong
+    return mIOU
+
 
 def main():
     args = parse_args()
@@ -70,6 +137,8 @@ def main():
         device = 'cuda'
     else:
         assert False, 'Using CPU.'
+    
+    assert args.teach_w + args.ground_w == 1.
     
     out_indices = [2, 5]
     use_stages = 2
@@ -87,7 +156,7 @@ def main():
         start_epoch = saved_model['last_epoch']
     else:
         name = f'lr={args.lr}-batch_size={args.batch_size}'
-        args.log_dir = f'{args.log_dir}/{name}'
+        args.log_dir = f'{args.log_dir}/{args.model_type}/{name}'
         epochs = args.epochs
         start_epoch = 1
 
@@ -111,57 +180,53 @@ def main():
     cfg = Config.fromfile(args.config)
 
     # ------------ build the models  --------------
-    # TODO: build teacher model
     if args.model_dir != '':
         simple_segvit = saved_model['simple_segvit']
+    elif args.model_type == 'AttUnet':
+        student = AttUNet().to(device)
+    elif args.model_type == 'Unet':
+        student = UNet().to(device)
+    elif args.model_type == 'fuse':
+        student1 = AttUNet().to(device)
+        student2 = SimpleSegViT(shrink_idx=args.shrink_idx,
+                                img_size=args.img_size,
+                                encoder_in_channels=args.encoder_in_channels,
+                                encoder_embed_dims=args.encoder_embed_dims,
+                                encoder_num_layers=args.encoder_num_layers,
+                                encoder_num_heads=args.encoder_num_heads,
+                                drop_path_rate=args.drop_path_rate,
+                                decoder_in_channels=args.decoder_in_channels,
+                                decoder_embed_dims=args.decoder_embed_dims,
+                                decoder_num_heads=args.decoder_num_heads,
+                                out_indices=out_indices,
+                                use_stages=use_stages,
+                                single=args.single
+                                ).to(device)
     else:
-        simple_segvit = SimpleSegViT(shrink_idx=args.shrink_idx,
-                                     img_size=args.img_size,
-                                     encoder_in_channels=args.encoder_in_channels,
-                                     encoder_embed_dims=args.encoder_embed_dims,
-                                     encoder_num_layers=args.encoder_num_layers,
-                                     encoder_num_heads=args.encoder_num_heads,
-                                     drop_path_rate=args.drop_path_rate,
-                                     decoder_in_channels=args.decoder_in_channels,
-                                     decoder_embed_dims=args.decoder_embed_dims,
-                                     decoder_num_heads=args.decoder_num_heads,
-                                     out_indices=out_indices,
-                                     use_stages=use_stages,
-                                     single=args.single
-                                     )
+        raise ValueError
     teacher = build_segmentor(cfg=cfg.model, test_cfg=cfg.get('test_cfg'))
     checkpoint = torch.load(args.checkpoint)
     del checkpoint['state_dict']['decode_head.loss_decode.criterion.empty_weight']
     teacher.load_state_dict(checkpoint['state_dict'])
 
     # --------- transfer to device ------------------------------------
-    simple_segvit = simple_segvit.to(device)
     teacher = teacher.to(device)
 
     # --------- load a dataset ------------------------------------
-    # TODO: build dataset
-    train_data = ade20k_dataset(args.config, mode='train')
+    train_data = ade20k_dataset(args.config, mode='train', crop_size=(args.img_size, args.img_size))
     train_loader = DataLoader(train_data,
                             num_workers=1,
                             batch_size=args.batch_size,
                             shuffle=True,
                             drop_last=True,
-                            pin_memory=True)
-    valid_data = ade20k_dataset(args.config, mode='valid')
+                            pin_memory=False)
+    valid_data = ade20k_dataset(args.config, mode='valid', crop_size=(args.img_size, args.img_size))
     valid_loader = DataLoader(valid_data,
                             num_workers=1,
                             batch_size=1,
                             shuffle=False,
                             drop_last=False,
-                            pin_memory=True)
-
-    # for img, label in train_loader:
-    #     break
-
-    for img, label in valid_loader:
-        train(teacher, simple_segvit, img, label, device)
-        break
-    
+                            pin_memory=False)
 
     # --------- optimizers ------------------------------------
     if args.optimizer == 'adam':
@@ -173,42 +238,100 @@ def main():
     else:
         raise ValueError(f'Unknown optimizer: {args.optimizer}')
 
-    # optimizer = args.optimizer(simple_segvit.parameters(), lr=args.lr)
+    if args.model_type == 'fuse':
+        optimizer = args.optimizer([student1.parameters()] + [student1.parameters()], lr=args.lr)
+    else:
+        optimizer = args.optimizer(student.parameters(), lr=args.lr)
 
     # --------- training loop ------------------------------------
-    # TODO
-    # best_test_acc = best_new_test_acc = 0
-    # for epoch in tqdm(range(start_epoch, epochs + 1)):
-    #     simple_segvit.train()
+    if args.model_type == 'fuse':
+        optimizer = args.optimizer([student1.parameters()] + [student1.parameters()], lr=args.lr)
+        best_test_mIoU1 = best_test_mIoU2 = 0
+    else:
+        optimizer = args.optimizer(student.parameters(), lr=args.lr)
+        best_test_mIoU = 0
 
-    #     epoch_loss = 0
+    for epoch in tqdm(range(start_epoch, epochs + 1)):
+        if args.model_type == 'fuse':
+            student1.train()
+            student2.train()
+            epoch_loss1 = epoch_loss2 = 0
+        else:
+            student.train()
+            epoch_loss = 0
 
-    #     for img in train_loader:
-    #         loss = train()
+        for img, label in train_loader:
+            img = img.to(device)
+            label = label.to(device)
+            if args.model_type == 'fuse':
+                loss1, loss2 = train(teacher, (student1, student2), img, label, optimizer, args)
+                epoch_loss1 += loss1
+                epoch_loss2 += loss2
+            else:
+                loss = train(teacher, student, img, label, optimizer, args)
+                epoch_loss += loss
 
-    #     with open(f'./{args.log_dir}/train_record.txt', 'a') as f:
-    #         f.write(f'[epoch: {epoch:02d}] loss: {epoch_loss:.5f}\n')
+        with open(f'./{args.log_dir}/train_record.txt', 'a') as f:
+            if args.model_type == 'fuse':
+                f.write(f'[epoch: {epoch:02d}] loss1: {epoch_loss1:.5f} | loss2: {epoch_loss2:.5f}]\n')
+            else:
+                f.write(f'[epoch: {epoch:02d}] loss: {epoch_loss:.5f}]\n')
 
-    #     if epoch % args.eval_freq == 0:
-    #         # --------- testing loop ------------------------------------
-    #         simple_segvit.eval()
-    #         test_acc = 0
-    #         for img in valid_loader:
-    #             pass
+        if epoch % args.eval_freq == 0:
+            # --------- testing loop ------------------------------------
+            if args.model_type == 'fuse':
+                student1.eval()
+                student2.eval()
+                test_mIoU1 = test_mIoU2 = 0
+            else:
+                student.eval()
+                test_mIoU = 0
+            for img, label in valid_loader:
+                img = img.to(device)
+                label = label.to(device)
+                if args.model_type == 'fuse':
+                    mIoU1, mIoU2 = pred(teacher, (student1, student2), img, label)
+                    test_mIoU1 += mIoU1
+                    test_mIoU2 += mIoU2
+                else:
+                    mIoU = pred(teacher, student, img, label)
+                    test_mIoU += mIoU
 
-    #         avg_test_acc = test_acc / len(test_loader)
+            with open(f'./{args.log_dir}/train_record.txt', 'a') as f:
+                if args.model_type == 'fuse':
+                    avg_test_mIoU1 = test_mIoU1 / len(valid_loader)
+                    avg_test_mIoU2 = test_mIoU2 / len(valid_loader)
+                    f.write(f'==================== valid mIoU1 = {avg_test_mIoU1:.5f}, mIoU2 = {avg_test_mIoU2:.5f} ====================\n')
+                else:
+                    avg_test_mIoU = test_mIoU / len(valid_loader)
+                    f.write(f'==================== valid mIoU = {avg_test_mIoU:.5f} ====================\n')
 
-    #         with open(f'./{args.log_dir}/train_record.txt', 'a') as f:
-    #             f.write(f'==================== valid mIoU = {avg_test_acc:.5f} ====================\n')
-
-    #         if avg_test_acc > best_test_acc:
-    #             best_test_acc = avg_test_acc
-    #             torch.save({
-    #                 'simple_segvit' : simple_segvit,
-    #                 'args': args,
-    #                 'las_epoch': epoch},
-    #                 f'{args.log_dir}/test_model.pth'
-    #             )
+            if args.model_type == 'fuse':
+                if avg_test_mIoU1 > best_test_mIoU1:
+                    best_test_mIoU1 = avg_test_mIoU1
+                    torch.save({
+                        'AttUnet' : student1,
+                        'args': args,
+                        'last_epoch': epoch},
+                        f'{args.log_dir}/test_model1.pth'
+                    )
+                if avg_test_mIoU2 > best_test_mIoU2:
+                    best_test_mIoU2 = avg_test_mIoU2
+                    torch.save({
+                        'simple_segvit' : student2,
+                        'args': args,
+                        'last_epoch': epoch},
+                        f'{args.log_dir}/test_model2.pth'
+                    )
+            else:
+                if avg_test_mIoU > best_test_mIoU:
+                    best_test_mIoU = avg_test_mIoU
+                    torch.save({
+                        f'{args.model_type}' : student,
+                        'args': args,
+                        'last_epoch': epoch},
+                        f'{args.log_dir}/test_model.pth'
+                    )
 
 
 if __name__ == '__main__':
